1ddc113408c124c115ff8d1e2c0b71ba14e20edc
wangwen1220 <wangwen1220@139.com>
2014-11-29 12:11:17 +0800

    'office'

--- js/main-debug.js
+++ js/main-debug.js
@@ -24,27 +24,55 @@
   $(function() {
     // 通用变量
     var $html = $('html');
-    var $header = $('header');
-    var $main = $('main');
+    // var $header = $('header');
+    // var $main = $('main');
+    var $swiper = $('#swiper-responsive');
+    var $counter = $('#js-counter');
 
     // 初始化 Swiper
-    $('.swiper-responsive').swiper({
+    $swiper.swiper({
       // pagination: '.pagination',
       // paginationClickable: true,
       onFirstInit: function(swiper) {
-        // todo
-        $('.swiper-responsive').removeClass('fn-vh');
+        var total = swiper.slides.length;
+        var index = swiper.activeIndex + 1;
+        var left = total - index;
+
+        $counter.children('.current').html(index);
+        $counter.children('.left').html(left);
+        $counter.children('.total').html(total);
+
+        $(swiper.container).removeClass('fn-vh');
+      },
+      onSlideChangeEnd: function(swiper) {
+        var index = swiper.activeIndex + 1;
+        var left = swiper.slides.length - index;
+
+        $counter.children('.current').html(index);
+        $counter.children('.left').html(left);
       }
     }).on('click', '.swiper-nav a', function(e) {
+      // console.log($(this))
       $(e.delegateTarget).data('swiper').swipeNext();
-    }).on('click', 'label', function(e) {
+    }).on('change', 'label', function(e) { // 这里有一个坑，用 click 事件，input 会冒泡，会导致触发两次
       var $this = $(this);
-      if ($this.parents('.swiper-slide').is(':last')) {
-        alert('test');
+      var $slide = $this.parents('.swiper-slide');
+      var swiper = $swiper.data('swiper');
+
+      if (swiper.activeIndex === swiper.slides.length - 1) {
+        console.log('submit');
+        return;
       }
-      setTimeout(function() {
-        $(e.delegateTarget).data('swiper').swipeNext();
-      }, 300);
+
+      $this.parent().animate({'right': '-50px'}, 500, function() {
+        $slide.animate({'right': 'auto'}, 0, function() {
+          swiper.swipeNext();
+        });
+      });
+
+      // setTimeout(function() {
+      //   $swiper.data('swiper').swipeNext();
+      // }, 300);
     });
   });
 })(jQuery);
